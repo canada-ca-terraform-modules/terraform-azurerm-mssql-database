6284f69357a48eaf18178596880a28c8eed44d27
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-07-12 18:49:08 +0000

    accept license type arg

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_mssql_database" "sql_db" {
   name                        = var.name
   server_id                   = var.server_id
   collation                   = var.collation
-  license_type                = "BasePrice"
+  license_type                = var.license_type
   max_size_gb                 = var.max_size_gb
   read_scale                  = var.read_scale
   sku_name                    = var.sku_name
