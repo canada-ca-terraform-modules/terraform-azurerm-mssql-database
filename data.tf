daddeac76ba6a74d2584cf616efa419a89bdc663
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-07-13 18:19:40 +0000

    data from arthur

--- data.tf
+++ data.tf
@@ -1,6 +1,18 @@
 locals {
   kv_name = var.kv_name
   kv_rg   = var.kv_rg
+  env_sku = substr(var.sku_name, 0, 5)
+  license_type = local.env_sku == "GP_S_" ? null : "BasePrice"
+  read_scale_out = false
+    
+    // Service tier prefixes
+    premium_prefix = "P"
+    business_prefix = "BC"
+    hyperscale_prefix = "HS"
+    general_serverless_prefix = "GP_S"
+
+    // Minimum time in minutes supported by Terraform configuration (only General Purpose Serverless)
+    min_auto_pause_supported = 60
 }
 
 data "azurerm_key_vault" "sqlhstkv" {
@@ -17,3 +29,4 @@ data "azurerm_storage_account" "storageaccountinfo" {
   name                = data.azurerm_key_vault_secret.storageaccountname.value
   resource_group_name = var.storageaccountinfo_resource_group_name
 }
+
