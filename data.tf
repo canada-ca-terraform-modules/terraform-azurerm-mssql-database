7b954b50016bf00c5fdaec2e5f0cae345bd4c63c
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-07-25 16:46:13 +0000

    remove sa

--- data.tf
+++ data.tf
@@ -19,14 +19,3 @@ data "azurerm_key_vault" "sqlhstkv" {
   name                = local.kv_name
   resource_group_name = local.kv_rg
 }
-
-# data "azurerm_key_vault_secret" "storageaccountname" {
-#   name         = "storageaccountname"
-#   key_vault_id = data.azurerm_key_vault.sqlhstkv.id
-# }
-
-# data "azurerm_storage_account" "storageaccountinfo" {
-#   name                = data.azurerm_key_vault_secret.storageaccountname.value
-#   resource_group_name = var.storageaccountinfo_resource_group_name
-# }
-
