aa91007422a842a0dc6023f476b84595d62bceee
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-08-23 15:17:03 +0000

    azurerm to az

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "serverless" {
         $secureString = ConvertTo-SecureString -String $env:ARM_CLIENT_SECRET -AsPlainText -Force
         $pscredential = New-Object System.Management.Automation.PSCredential($env:ARM_CLIENT_ID, $secureString)
         Connect-AzAccount -ServicePrincipal -Credential $pscredential -TenantId $env:ARM_TENANT_ID -Subscription $env:ARM_SUBSCRIPTION_ID | Out-Null
-        Set-AzureRmSqlDatabaseBackupLongTermRetentionPolicy -ResourceGroupName "${var.resource_group_name}" -ServerName "${var.server_name}" -DatabaseName "${var.name}" -RemovePolicy
+        Set-AzSqlDatabaseBackupLongTermRetentionPolicy -ResourceGroupName "${var.resource_group_name}" -ServerName "${var.server_name}" -DatabaseName "${var.name}" -RemovePolicy
         EOT
     interpreter = ["pwsh", "-Command"]
   }
