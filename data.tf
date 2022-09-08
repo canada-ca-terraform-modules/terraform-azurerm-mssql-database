7c8d21972379cb72081df2a2ff99ed170b77645d
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-09-08 17:56:16 +0000

    license type

--- data.tf
+++ data.tf
@@ -1,12 +1,11 @@
 locals {
-  license_type = substr(var.sku_name, 3, 1) == "S" ? "LicenseIncluded" : "BasePrice"
   read_scale_out = false
-    
-    // Service tier prefixes
-    premium_prefix = "P"
-    business_prefix = "BC"
-    hyperscale_prefix = "HS"
-    general_serverless_prefix = "GP_S"
+  // Service tier prefixes
+  premium_prefix = "P"
+  business_prefix = "BC"
+  hyperscale_prefix = "HS"
+  general_serverless_prefix = "GP_S"
+  license_type = substr(var.sku_name, 0, length(local.general_serverless_prefix)) == local.general_serverless_prefix ? "LicenseIncluded" : "BasePrice"
 
     // Minimum time in minutes supported by Terraform configuration (only General Purpose Serverless)
     min_auto_pause_supported = 60
