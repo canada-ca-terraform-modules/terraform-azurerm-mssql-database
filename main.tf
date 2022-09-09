2af20e46096f8c2c81c2b1d10d7297a37d3e50ce
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-09-09 19:23:34 +0000

    tigger error

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "null_resource" "this" {
 }
 
 resource "azurerm_mssql_database" "sql_db" {
+  adfnhasf,sajflajflk
   name                        = var.name
   server_id                   = var.server_id 
   collation                   = var.collation
