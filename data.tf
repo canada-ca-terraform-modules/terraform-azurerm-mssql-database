522462a53896d623644feb674d8a18545d4d0735
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-09-20 17:54:27 +0000

    replace null resource

--- data.tf
+++ data.tf
@@ -6,11 +6,21 @@ locals {
   hyperscale_prefix = "HS"
   general_serverless_prefix = "GP_S"
   license_type = substr(var.sku_name, 0, length(local.general_serverless_prefix)) == local.general_serverless_prefix ? "LicenseIncluded" : "BasePrice"
-    // Minimum time in minutes supported by Terraform configuration (only General Purpose Serverless)
-    min_auto_pause_supported = 60
+  // Minimum time in minutes supported by Terraform configuration (only General Purpose Serverless)
+  min_auto_pause_supported = 60
+
+  script = templatefile("${path.module}/ltrconfig.ps1.tftpl", { 
+      db_name             = var.name
+      server_name         = var.mss
+      resource_group_name = var.resource_group_name
+    }) 
+  }
 }
 
 data "azurerm_key_vault" "sqlhstkv" {
   name                = var.kv_name
   resource_group_name = var.kv_rg
 }
+
+
+
