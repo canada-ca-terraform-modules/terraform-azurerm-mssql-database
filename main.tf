5fe791f0e21885a132b835de405605c809d53e76
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-09-22 12:34:02 +0000

    Cleanup

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "time_sleep" "this" {
   create_duration = "5m"
 }
 
-
 resource "null_resource" "this" {
   provisioner "local-exec" {
     command = "cat ${local.script}"
@@ -12,39 +11,20 @@ resource "null_resource" "this" {
   ]
 }
 
-
-
-# resource "null_resource" "this" {
-#   depends_on = [time_sleep.this]
-#   count = substr(var.sku_name, 0, length(local.general_serverless_prefix)) == local.general_serverless_prefix ? 1 : 0
-  
-#   provisioner "local-exec" {
-#     command     = <<EOT
-#         $secureString = ConvertTo-SecureString -String $env:ARM_CLIENT_SECRET -AsPlainText -Force
-#         $pscredential = New-Object System.Management.Automation.PSCredential($env:ARM_CLIENT_ID, $secureString)
-#         Connect-AzAccount -ServicePrincipal -Credential $pscredential -TenantId $env:ARM_TENANT_ID -Subscription $env:ARM_SUBSCRIPTION_ID | Out-Null
-#         if (Get-AzSqlDatabase -ResourceGroupName "${var.resource_group_name}" -ServerName "${var.server_name}" | where DatabaseName -eq "${var.name}") {
-#           Set-AzSqlDatabaseBackupLongTermRetentionPolicy -ResourceGroupName "${var.resource_group_name}" -ServerName "${var.server_name}" -DatabaseName "${var.name}" -RemovePolicy
-#         }
-#         Disconnect-AzAccount | Out-Null
-#         EOT
-#     interpreter = ["pwsh", "-Command"]
-#   }
-# }
-
 resource "azurerm_mssql_database" "sql_db" {
   name                        = var.name
   server_id                   = var.server_id 
   collation                   = var.collation
   max_size_gb                 = var.max_size_gb
   sku_name                    = var.sku_name
+
   create_mode                 = var.create_mode
   creation_source_database_id = var.creation_source_database_id
   restore_dropped_database_id = var.restore_dropped_database_id
-  recover_database_id         = var.recover_database_id
-  elastic_pool_id             = var.elastic_pool_id
-  restore_point_in_time       = var.restore_point_in_time
+  recover_database_id         = var.recover_database_idrestore_point_in_time       = var.restore_point_in_time
+  
   sample_name                 = var.sample_name
+  elastic_pool_id             = var.elastic_pool_id
 
   // SERVERLESS
   auto_pause_delay_in_minutes = (
@@ -76,6 +56,7 @@ resource "azurerm_mssql_database" "sql_db" {
           local.business_prefix ? var.zone_redundant : null
   )
 
+  // STR
   short_term_retention_policy {
       retention_days = var.str_days
   }
@@ -88,12 +69,12 @@ resource "azurerm_mssql_database" "sql_db" {
       monthly_retention = var.ltr_monthly_retention
       yearly_retention  = var.ltr_yearly_retention
       week_of_year      = var.ltr_week_of_year
-    
     }
   }
   
   tags       = merge(var.tags, 
-                    {"prefix" = substr(var.sku_name, 0, length(local.general_serverless_prefix))})
+                    {"prefix" = substr(var.sku_name, 0, length(local.general_serverless_prefix))}
+              )
   depends_on = [
     null_resource.this
   ]
@@ -105,4 +86,9 @@ resource "azurerm_mssql_database_extended_auditing_policy" "this" {
   storage_account_access_key = var.sa_primary_access_key
   retention_in_days      = var.retention_days
   log_monitoring_enabled = true
+  
+  depends_on = [
+    
+    azurerm_mssql_database.sql_db
+  ]
 }
