a20f7303d410057863e6eb5c67ed72d63b0aeec9
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-09-28 15:23:44 +0000

    pre commit

--- data.tf
+++ data.tf
@@ -1,20 +1,20 @@
 locals {
   read_scale_out = false
   // Service tier prefixes
-  premium_prefix = "P"
-  business_prefix = "BC"
-  hyperscale_prefix = "HS"
+  premium_prefix            = "P"
+  business_prefix           = "BC"
+  hyperscale_prefix         = "HS"
   general_serverless_prefix = "GP_S"
 
   // Minimum time in minutes supported by Terraform configuration (only General Purpose Serverless)
   min_auto_pause_supported = 60
 
-  script = templatefile("${path.module}/files/ltrconfig.ps1.tftpl", { 
-      db_name             = var.name
-      server_name         = var.server_name
-      resource_group_name = var.resource_group_name
-    }) 
-  }
+  script = templatefile("${path.module}/files/ltrconfig.ps1.tftpl", {
+    db_name             = var.name
+    server_name         = var.server_name
+    resource_group_name = var.resource_group_name
+  })
+}
 
 data "azurerm_key_vault" "sqlhstkv" {
   name                = var.kv_name
--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "time_sleep" "this" {
 }
 
 resource "null_resource" "this" {
-  count = substr(var.sku_name,0,4) == local.general_serverless_prefix ? 0 : 1
+  count = substr(var.sku_name, 0, 4) == local.general_serverless_prefix ? 0 : 1
   provisioner "local-exec" {
     command = "cat ${local.script}"
   }
@@ -13,54 +13,54 @@ resource "null_resource" "this" {
 }
 
 resource "azurerm_mssql_database" "sql_db" {
-  name                        = var.name
-  server_id                   = var.server_id 
-  collation                   = var.collation
-  max_size_gb                 = var.max_size_gb
-  sku_name                    = var.sku_name
+  name        = var.name
+  server_id   = var.server_id
+  collation   = var.collation
+  max_size_gb = var.max_size_gb
+  sku_name    = var.sku_name
 
   create_mode                 = var.create_mode
   creation_source_database_id = var.creation_source_database_id
   restore_dropped_database_id = var.restore_dropped_database_id
   recover_database_id         = var.recover_database_id
   restore_point_in_time       = var.restore_point_in_time
-  
-  sample_name                 = var.sample_name
-  elastic_pool_id             = var.elastic_pool_id
+
+  sample_name     = var.sample_name
+  elastic_pool_id = var.elastic_pool_id
 
   // SERVERLESS
   auto_pause_delay_in_minutes = (
-      substr(var.sku_name, 0, length(local.general_serverless_prefix)) == 
-          local.general_serverless_prefix && 
-          var.auto_pause_delay_in_minutes >= local.min_auto_pause_supported ?
-          var.auto_pause_delay_in_minutes : null
+    substr(var.sku_name, 0, length(local.general_serverless_prefix)) ==
+    local.general_serverless_prefix &&
+    var.auto_pause_delay_in_minutes >= local.min_auto_pause_supported ?
+    var.auto_pause_delay_in_minutes : null
   )
   min_capacity = (
-      substr(var.sku_name, 0, length(local.general_serverless_prefix)) == 
-          local.general_serverless_prefix ? var.min_capacity : null
+    substr(var.sku_name, 0, length(local.general_serverless_prefix)) ==
+    local.general_serverless_prefix ? var.min_capacity : null
   )
 
   // HYPERSCALE
   read_replica_count = (
-      substr(var.sku_name, 0, length(local.hyperscale_prefix)) == 
-          local.hyperscale_prefix ? var.read_replica_count : null
+    substr(var.sku_name, 0, length(local.hyperscale_prefix)) ==
+    local.hyperscale_prefix ? var.read_replica_count : null
   )
 
   // BC & PREMIUM
   read_scale = (
-      substr(var.sku_name, 0, length(local.premium_prefix)) == local.premium_prefix || 
-      substr(var.sku_name, 0, length(local.business_prefix)) == 
-          local.business_prefix ? var.read_scale : null
+    substr(var.sku_name, 0, length(local.premium_prefix)) == local.premium_prefix ||
+    substr(var.sku_name, 0, length(local.business_prefix)) ==
+    local.business_prefix ? var.read_scale : null
   )
   zone_redundant = (
-      substr(var.sku_name, 0, length(local.premium_prefix)) == local.premium_prefix || 
-      substr(var.sku_name, 0, length(local.business_prefix)) == 
-          local.business_prefix ? var.zone_redundant : null
+    substr(var.sku_name, 0, length(local.premium_prefix)) == local.premium_prefix ||
+    substr(var.sku_name, 0, length(local.business_prefix)) ==
+    local.business_prefix ? var.zone_redundant : null
   )
 
   // STR
   short_term_retention_policy {
-      retention_days = var.str_days
+    retention_days = var.str_days
   }
 
   // LTR
@@ -73,10 +73,10 @@ resource "azurerm_mssql_database" "sql_db" {
       week_of_year      = var.ltr_week_of_year
     }
   }
-  
-  tags       = merge(var.tags, 
-                    {"prefix" = substr(var.sku_name, 0, length(local.general_serverless_prefix))}
-              )
+
+  tags = merge(var.tags,
+    { "prefix" = substr(var.sku_name, 0, length(local.general_serverless_prefix)) }
+  )
   depends_on = [
     null_resource.this
   ]
@@ -86,9 +86,9 @@ resource "azurerm_mssql_database_extended_auditing_policy" "this" {
   database_id                = azurerm_mssql_database.sql_db.id
   storage_endpoint           = var.sa_primary_blob_endpoint
   storage_account_access_key = var.sa_primary_access_key
-  retention_in_days      = var.retention_days
-  log_monitoring_enabled = true
-  
+  retention_in_days          = var.retention_days
+  log_monitoring_enabled     = true
+
   depends_on = [
     azurerm_mssql_database.sql_db
   ]
