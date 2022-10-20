ed19f5cf5ff5d6fa3e6f61dfb70cf2dd0c1d5ccf
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-10-20 21:07:21 +0000

    remove provider

--- main.tf
+++ main.tf
@@ -109,10 +109,6 @@ resource "azurerm_mssql_job_credential" "this" {
   password     = var.job_agent_credentials.password
 }
 
-provider "mssql" {
-  debug = "false"
-}
-
 resource "mssql_login" "this" {
   server {
     host = var.server_name
